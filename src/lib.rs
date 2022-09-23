355c9e6de4daafc9a5d89b0ea36fe044bb67e138
gcxfd <i@rmw.link>
2022-09-23 15:46:38 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,14 @@
-#![feature(new_uninit)]
-
 use blake3::Hasher;
 use core::hash::Hasher as _;
 use std::convert::TryInto;
-use twox_hash::xxh3::{hash64, Hash64};
+use xxhash_rust::xxh3::xxh3_64;
 
 const LEN_U64: usize = std::mem::size_of::<u64>();
-const H64_SEED: u64 = 1;
+const H64_SEED: u64 = 23456789;
 
 #[inline]
 pub fn hash_data_secret(secret: &[u8], data: &[u8]) -> u64 {
-  let mut h64 = Hash64::with_seed(H64_SEED);
+  let mut h64 = xxhash_rust::xxh3::Xxh3Builder::with_seed(H64_SEED)::build_hasher();
   h64.write(data);
   h64.write(secret);
   h64.finish()
@@ -23,10 +21,10 @@ macro_rules! xor {
 }
 
 pub fn encrypt(secret: &[u8], iv: &[u8], data: &[u8]) -> Box<[u8]> {
-  let hash = hash64(data);
+  let hash = xxh3_64(data);
 
   let out_len = LEN_U64 + data.len();
-  let mut out = unsafe { Box::<[u8]>::new_uninit_slice(out_len).assume_init() };
+  let mut out = Box::new([u8; out_len]);
   let out_data = &mut out[LEN_U64..];
 
   Hasher::new()
@@ -36,20 +34,20 @@ pub fn encrypt(secret: &[u8], iv: &[u8], data: &[u8]) -> Box<[u8]> {
     .finalize_xof()
     .fill(out_data);
 
-  xor!(out_data, data);
+    xor!(out_data, data);
 
-  let hash = hash_data_secret(out_data, secret) ^ hash;
+    let hash = hash_data_secret(out_data, secret) ^ hash;
 
-  out[..LEN_U64].clone_from_slice(&hash.to_le_bytes());
+    out[..LEN_U64].clone_from_slice(&hash.to_le_bytes());
 
-  out
+    out
 }
 
 pub fn decrypt(secret: &[u8], iv: &[u8], data: &[u8]) -> Option<Box<[u8]>> {
   let ed = &data[LEN_U64..];
   let hash = u64::from_le_bytes(data[..LEN_U64].try_into().unwrap()) ^ hash_data_secret(ed, secret);
   let out_len = data.len() - LEN_U64;
-  let mut out = unsafe { Box::<[u8]>::new_uninit_slice(out_len).assume_init() };
+  let mut out = Box::new([u8; out_len]);
 
   Hasher::new()
     .update(&hash.to_le_bytes())
@@ -58,11 +56,11 @@ pub fn decrypt(secret: &[u8], iv: &[u8], data: &[u8]) -> Option<Box<[u8]>> {
     .finalize_xof()
     .fill(&mut out);
 
-  xor!(out, ed);
+    xor!(out, ed);
 
-  if hash64(&out) != hash {
-    None
-  } else {
-    Some(out)
-  }
+    if xxh3_64(&out) != hash {
+      None
+    } else {
+      Some(out)
+    }
 }
